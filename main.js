94b1e871f763675f896ab6b2655950e318276816
smallnumbers0 <jchoi53@mail.ccsf.edu>
2025-08-03 11:47:01 -0700

    display pokemon naames in text-display

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 class PokeTyping {
     constructor() {
         this.pokemonNames = [];
+        this.text = "";
 
     }
     async fetchPokemonNames() {
         try {
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000');
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon');
             const data = await response.json();
-            this.pokemonNames = data.results.map(pokemon => pokemon.name);
+            let pokemonArr = data.results.map(pokemon => pokemon.name);
+            this.pokemonNames = this.getRandomPokemonNames(pokemonArr, 100);
+            this.text = this.pokemonNames.join(' ');
+            this.displayText();
             console.log(this.pokemonNames);
+            
         } catch (error) {
-            console.error('Error fetching Pokémon names:', error);
+            console.error(error);
         }
+    }
+
+    getRandomPokemonNames(pokemonArr, count) {
+        let randomNum = 0;
+        let randomPokemonNames = [];
+        for (let i = 0; i < count; i++) {
+            randomNum = Math.floor(Math.random() * pokemonArr.length);
+            randomPokemonNames.push(pokemonArr[randomNum]);
+        }
+        return randomPokemonNames;
+    }
+
+    displayText() {
+        const textElement = document.getElementById('text-to-type');
+        textElement.textContent = this.text;
     }
 
 }
 
 const newGame = new PokeTyping();
-newGame.fetchPokemonNames();
+newGame.fetchPokemonNames()
